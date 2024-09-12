05935dd83904ef39b03823f3dfb90346ebcd4f7a
ArturVinicius <lartur671@gmail.com>
2024-09-12 15:08:28 -0300

    ft(add-service):service de categoria criado e implementado.

---
+++ src/services/categoryService.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/category';
+
+export const getCategory = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+};
+
+export const createCategory = async (category) => {
+    const response = await axios.post(API_URL, category);
+    return response.data;
+};
+
+export const updateCategory = async (id, category) => {
+    const response = await axios.put(`${API_URL}/${id}`, category);
+    return response.data;
+};
+
+export const deleteCategory = async (id) => {
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response.data;
+};
