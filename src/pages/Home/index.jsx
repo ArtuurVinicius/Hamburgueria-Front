1a2141f6d6c0af8e7a27e2117434c1cff92eb160
ArturVinicius <lartur671@gmail.com>
2024-11-18 22:48:10 -0300

    fix(add-new-home):refazendo a home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,18 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import './style.css';
 
 function Home() {
-  const [count, setCount] = useState(0);
   const navigate = useNavigate();
 
   return (
-    <div className='container' id='home'>
-      <h1 id='pName'>Hamburgueria</h1>
+    <div className="sidebar">
+      <h1 id="pName">Hamburgueria</h1>
       <button onClick={() => navigate('/categoria-alimentos')}>Cadastro de Categorias de Alimentos</button>
       <button onClick={() => navigate('/cadastro-produtos')}>Cadastro de Produtos</button>
       <button onClick={() => navigate('/cadastro-precos')}>Cadastro e Vinculação de Preços aos Produtos</button>
       <button onClick={() => navigate('/cadastro-clientes')}>Cadastro de Clientes</button>
     </div>
-
   );
 }
 
